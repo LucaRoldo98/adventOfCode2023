0ca5b8363cc8c022c25f01327a1b6ee36e293e28
LucaRoldo98 <roldoluca98@gmail.com>
2024-01-03 17:02:16 +0100

    day 23 part 1

--- Day 23/solution1.py
+++ Day 23/solution1.py
@@ -1,50 +1,63 @@
+from heapq import heappop, heappush
+
 filename = "input.txt"
 
-bricks = []
+map = []
+sr = sc = -1
+er = ec = -1
 
 with open(filename) as text:
     for i,row in enumerate(text):
-        x = row.strip().split("~")
-        bricks.append((tuple(map(int, x[0].split(","))), tuple(map(int, x[1].split(",")))))
-
-maxx = max(bricks, key=lambda x: x[1][0])[1][0]
-maxy = max(bricks, key=lambda x: x[1][1])[1][1]
-
-bricks = sorted(bricks, key = lambda x: (x[1][2], x[0][2]))
-
-grid = [[(0, -1) for _ in range(maxy + 1)] for _ in range(maxx + 1)]
-
-essentials = set()
-
-# Compute the falling bricks
-for i,(spoint, epoint) in enumerate(bricks): 
-    
-    sx, sy, sz = spoint
-    ex, ey, ez = epoint
-
-    currBottom = 0
-    touchBricks = set()
-
-    for x in range(sx, ex + 1):
-        for y in range(sy, ey + 1):
-            prevHeight, id = grid[x][y] 
-            if prevHeight > currBottom:
-                touchBricks = set()
-                touchBricks.add(id)
-                currBottom = prevHeight
-            elif prevHeight == currBottom:
-                touchBricks.add(id)
-
-    if len(touchBricks) == 1:
-        for el in touchBricks:
-            essentials.add(el)
-
-    for x in range(sx, ex + 1):
-        for y in range(sy, ey + 1):
-            grid[x][y] = (currBottom + (ez - sz) + 1, i)
-
-print(len(bricks) - len(essentials) + 1)
-
-                    
+        map.append(list(row.strip()))
                 
+nRows = len(map)
+nCols = len(map[0])
+        
+for j,ch in enumerate(map[0]):
+    if ch == ".":
+        sr, sc = (0,j)
+        break
+    
+for j,ch in enumerate(map[nRows - 1]):
+    if ch == ".":
+        er, ec = (nRows - 1,j)
+        break
+            
+pq = [(0, sr, sc, set())]
+dirs = [(-1, 0), (0,1), (1,0), (0,-1)]
+
+pathLengths = []
+
+while pq:
+    
+    distance, r, c, visited = heappop(pq)
+    
+    if (r,c) in visited:
+        continue
+    
+    if (r,c) == (er, ec):
+        pathLengths.append(distance)
+        continue
+    
+    
+    newVisited = visited.copy()
+    newVisited.add((r,c))
+    
+    if map[r][c] == ">":
+        heappush(pq, (distance + 1, r, c + 1, newVisited))
+    elif map[r][c] == "v":
+        heappush(pq, (distance + 1, r + 1, c, newVisited))
+    elif map[r][c] == "<":
+        heappush(pq, (distance + 1, r, c - 1, newVisited))
+    elif map[r][c] == "^":
+        heappush(pq, (distance + 1, r - 1, c, newVisited))
+    else:
+        for dr, dc in dirs:
+            nr, nc = r + dr, c + dc
+            if nr < 0 or nr >= nRows or nc < 0 or nc >= nCols or map[nr][nc] == "#" or (nr,nc) in visited:
+                continue
+            heappush(pq, (distance + 1, nr, nc, newVisited))
+        
+print(pathLengths) 
+print(max(pathLengths))           
         
